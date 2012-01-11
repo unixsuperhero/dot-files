d5de64cf580f190e36a91c791442680046a26843
Dante (The Bawse) Elrik and Josh (The Jap) Toyota <dev+elrikdante+jearsh@saveology.com>
2012-01-11 14:10:54 -0500

    Using volatile aliases to save common command usages.

--- bash/home/aliases.sh
+++ bash/home/aliases.sh
@@ -1,3 +1,49 @@
+  # -------------------------------------------------------------------
+  # SO THAT I KNOW THE COMMON ARGUMENTS I LIKE TO USE WITH THE COMMANDS
+  # -------------------------------------------------------------------
+
+cat <<"Explanation"
+
+  Command Templates (Much like a command cheat sheet.)
+
+    The following are aliases that are not public. (So to speak, change
+  phrasing here...)
+
+    They are aliases with the sole purpose of showing me the different
+  possible groupings of options available for each command.
+
+Explanation
+
+    alias_name=git_log_cheat_sheet
+    alias $alias_name="git log --summary"
+    alias $alias_name="git log --name-only"
+    alias $alias_name="git log --name-status"
+    unalias $alias_name
+
+    alias_name=ls_cheat_sheet
+    alias $alias_name="ls -aCFG" # 2010-06-15 20:24:24
+    alias $alias_name="ls -ACFG" # 2010-06-16 17:27:00 # after learning that -A == -a except that it excludes '.' and '..'
+    unalias $alias_name
+
+    alias_name=screen_cheat_sheet
+    alias $alias_name="screen -ls"          # $eg> sls              # list screen sessions
+    alias $alias_name="screen -e^Ss"        # $eg> subscreen        # not sure the purpose of this one
+    alias $alias_name="screen -S"           # $eg> sS work          # start a new screen session, calling it "work"    #### start a new session with the given name
+    alias $alias_name="screen -e^Ss -S"     # $eg> subsS daemons    # start "daemons", a new session, using ^s as the command key
+    alias $alias_name="screen -x"           # $eg> sx work          # open "work", an existing screen session
+    alias $alias_name="screen -e^Ss -x"     # $eg> subsx daemons    # open "daemons", with ^s as the command key
+    unalias $alias_name
+
+    alias_name=egrep_cheat_sheet
+    alias $alias_name="egrep --color -I"
+    alias $alias_name="egrep -IRin"
+    alias $alias_name="egrep -IRil"
+    unalias $alias_name
+
+    alias_name=mail_cheat_sheet
+    alias $alias_name="mail -f ~/mbox"
+    unalias $alias_name
+
 
 # ----------
 #  COMMANDS
@@ -100,34 +146,17 @@
 
 # [ GIT ALIASES ]
 
+  alias git_aliases="alias | grep git"
   alias gsf="gs"
-
-# [ GIT REMINDER ALIASES ]
-
-  # -------------------------------------------------------------------
-  # SO THAT I KNOW THE COMMON ARGUMENTS I LIKE TO USE WITH THE COMMANDS
-  # -------------------------------------------------------------------
-
-    alias gitlogalias="git log --summary"
-    alias gitlogalias="git log --name-only"
-    alias gitlogalias="git log --name-status"
+  alias gss="git status -s"
 
 # -------------------------------------------------------------------------
 # THE FOLLOWING ARE NOTES ABOUT ALIASES IN GENERAL OR SPECIFIC TO A COMMAND
 # -------------------------------------------------------------------------
 
-  ### the following two alias ls= cmds should be for lsc
-  # 2010-06-15 20:24:24
-  # current alias ls
-
-    alias sample_lsc='ls -aCGF'
-
-  # 2010-06-16 17:27:00 
-  # after learning that -A == -a except that it excludes '.' and '..'
-
-    alias sample_lsc='ls -ACGF'
 
 alias turl='curl --socks5-hostname localhost:51892'
 alias deliver="gp && gp staging && heroku rake db:migrate --app saveology-admin-staging && heroku restart --app saveology-admin-staging"
 alias gac="git add . ; git commit -v"
 alias rdm="bundle exec rake db:migrate db:test:prepare"
+alias columnize='column -c $COLUMNS | column -t'
