4c91ecf9b5052c0c134dcf993a70dc1cae6757bb
Joshua Toyota <jearsh@gmail.com>
2012-06-14 10:55:28 -0400

    cleanup of bashrc files, and more

---
+++ uninstall.1339505082.sh
@@ -0,0 +1,6 @@
+rm /Users/jearsh/.bash_profile
+mv -v backup/home/1339505082/.bash_profile /Users/jearsh/.bash_profile
+rm /Users/jearsh/.bashrc.local
+mv -v backup/home/1339505082/.bashrc.local /Users/jearsh/.bashrc.local
+rm /Users/jearsh/hashrocket
+mv -v backup/home/1339505082/hashrocket /Users/jearsh/
