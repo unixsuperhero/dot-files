e15dafb452668563356632232607b861a213b5fb
Joshua Toyota <jearsh@gmail.com>
2015-02-04 17:45:16 -0500

    customize mkdir command to auto -pv and cd into it

---
+++ fish/config/fish/functions/mkdir.fish
@@ -0,0 +1,11 @@
+function mkdir -d "Create a directory and set CWD"
+        command mkdir -pv $argv
+        if test $status = 0
+                switch $argv[(count $argv)]
+                        case '-*'
+                        case '*'
+                                cd $argv[(count $argv)]
+                                return
+                end
+        end
+end
