e744039bd9fda92ef2eaad6da5a038428a3aff44
Joshua Toyota <jearsh@gmail.com>
2015-02-25 21:08:26 -0500

    fish function: use git cherry to look at commits that are in one branch and not another, based on changes, not shas

---
+++ fish/config/fish/functions/commits_in_b_not_in_a.fish
@@ -0,0 +1,3 @@
+function commits_in_b_not_in_a
+	if test (count $argv) -gt 1; git cherry -v $argv[1] $argv[2] | grep -C1 '^[+]' | uniq | sed -E 's/^--//;s/^(.{9})[^[:space:]]+/\1/'; else; git cherry -v $argv[1] | grep -C1 '^[+]' | uniq | sed -E 's/^--//;s/^(.{9})[^[:space:]]+/\1/'; end; 
+end
