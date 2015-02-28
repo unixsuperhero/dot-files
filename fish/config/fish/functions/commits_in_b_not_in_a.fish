95651e809127195966e808f21de15f51bab25e21
Joshua Toyota <jearsh@gmail.com>
2015-02-28 11:13:49 -0500

    lots of changes

--- fish/config/fish/functions/commits_in_b_not_in_a.fish
+++ fish/config/fish/functions/commits_in_b_not_in_a.fish
@@ -1,3 +1,7 @@
 function commits_in_b_not_in_a
-	if test (count $argv) -gt 1; git cherry -v $argv[1] $argv[2] | grep -C1 '^[+]' | uniq | sed -E 's/^--//;s/^(.{9})[^[:space:]]+/\1/'; else; git cherry -v $argv[1] | grep -C1 '^[+]' | uniq | sed -E 's/^--//;s/^(.{9})[^[:space:]]+/\1/'; end; 
+  if test (count $argv) -gt 1
+    git cherry -v $argv[1] $argv[2] | grep -C1 '^[+]' | uniq | sed -E 's/^--//;s/^(.{9})[^[:space:]]+/\1/'
+  else
+    git cherry -v $argv[1] | grep -C1 '^[+]' | uniq | sed -E 's/^--//;s/^(.{9})[^[:space:]]+/\1/'
+  end
 end
